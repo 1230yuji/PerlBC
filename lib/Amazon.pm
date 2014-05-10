489c76897f06b5052a813c78240b85ff0a29467b
YutaKosuki <bambiyuta@gmail.com>
2014-05-10 13:38:22 +0900

    add_cartメソッド追加

--- lib/Amazon.pm
+++ lib/Amazon.pm
@@ -2,14 +2,33 @@
 
 use strict;
 use warnings;
+use Data::Dumper;
 
 sub new {
     my ( $class ) = @_;
-    bless{}, $class;
+
+    #warn Dumper ITEMS;
+
+    bless{
+        cart => {},
+        items => {name => 'Perfect PHP',  price => 3600 , release => '2010/11', stock => 2 }
+    }, $class;
 }
 
 sub add_cart {
-    return 1;
+    my ($self, $name , $num_purchases) = @_;
+    #warn Dumper $self->{items};
+    for my $item ($self->{items}) {
+        #warn Dumper $item;
+        if($self->{items}{name} eq $name) {
+            #一致していたらカートに入れる
+            $self->{cart}->{$item->{name}} = $item;
+
+            return 1;
+        }else {
+            die 'no such item';
+        }
+    }
 }
 
 1;
